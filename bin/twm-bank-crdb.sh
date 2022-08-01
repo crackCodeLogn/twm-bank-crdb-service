d122ff3aa153178caedb671dcb9b63730cd1d0dd
Vivek <vivektheintel@gmail.com>
2022-08-01 13:48:32 +0530

    Updating proto jar and adding multi host eureka

--- bin/twm-bank-crdb.sh
+++ bin/twm-bank-crdb.sh
@@ -5,5 +5,13 @@ JAVA_PARAM="-Xmx501m"
 BIN_PATH=$TWM_HOME_PARENT/TWM/$APP_NAME/bin     #TWM-HOME-PARENT :: exported in .bashrc
 JAR_PATH=$BIN_PATH/../target/$APP_NAME-$APP_VERSION.jar
 
-echo "Starting '$APP_NAME' with java param: '$JAVA_PARAM', at '$JAR_PATH'"
-java $JAVA_PARAM -jar $JAR_PATH
+if [[ $# -eq 0 ]]; then
+    EUREKA_URI="http://localhost:2012/eureka"
+else
+    EUREKA_URI="http://192.168.1.$1:2012/eureka"
+fi
+
+APP_PARAMS="-DEUREKA_URI=$EUREKA_URI"
+
+echo "Starting '$APP_NAME' with java param: '$JAVA_PARAM', app param: '$APP_PARAMS', at '$JAR_PATH'"
+java $JAVA_PARAM $APP_PARAMS -jar $JAR_PATH
